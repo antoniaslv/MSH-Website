eb7df83d7eaf53cfd7a808466e6a7e9d4c213bba
antoniaslv <154865592+antoniaslv@users.noreply.github.com>
2024-02-24 13:09:03 +0100

    Add files via upload

---
+++ website.js
@@ -0,0 +1,13 @@
+
+window.onclick = function(event) {
+  if (!event.target.matches('#privateButton')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === 'block') {
+        openDropdown.style.display = 'none';
+      }
+    }
+  }
+}
+
